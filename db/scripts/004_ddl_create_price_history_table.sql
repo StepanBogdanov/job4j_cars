0b72a7a484a4415e726ae6e0bfadcace6e4e1971
Stepan Bogdanov <bogstepan@gmail.com>
2023-07-09 23:28:36 +0300

    2. ManyToMany [#331986]

--- db/scripts/004_ddl_create_price_history_table.sql
+++ db/scripts/004_ddl_create_price_history_table.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS price_history
    id               SERIAL PRIMARY KEY,
    before           BIGINT not null,
    after            BIGINT not null,
-   created          TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+   created          TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
    auto_post_id     INT REFERENCES auto_posts(id)
 );
