22e7dba96091f9a521d0a5735d03dae375d06de2
Stepan Bogdanov <bogstepan@gmail.com>
2023-07-09 23:23:47 +0300

    1. ToMany [#301848 #428365]

--- db/scripts/002_ddl_create_auto_posts_table.sql
+++ db/scripts/002_ddl_create_auto_posts_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS auto_posts
 (
     id              SERIAL PRIMARY KEY,
     description     varchar not null,
-    created         timestamp now(),
+    created         TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     auto_user_id    int REFERENCES auto_users(id)
 );
