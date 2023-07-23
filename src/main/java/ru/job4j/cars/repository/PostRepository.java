7895e8f1d702d7c20ab8c0711e79586f0c7189a0
Stepan Bogdanov <bogstepan@gmail.com>
2023-07-23 22:13:17 +0300

    1. Фильтры для объявлений машин [#4745 #431356]

--- src/main/java/ru/job4j/cars/repository/PostRepository.java
+++ src/main/java/ru/job4j/cars/repository/PostRepository.java
@@ -12,18 +12,21 @@ public class PostRepository {
 
     private CrudRepository crudRepository;
 
+    private static final String FINDBYCONDITION = "SELECT DISTINCT p FROM Post p JOIN FETCH p.history "
+            + "LEFT JOIN FETCH p.participates WHERE %s ORDER BY p.id";
+
     public List<Post> findNewPosts() {
-        return crudRepository.query("SELECT DISTINCT p FROM Post p JOIN FETCH p.history LEFT JOIN FETCH p.participates WHERE p.created >= oneDay ORDER BY p.id",
+        return crudRepository.query(String.format(FINDBYCONDITION, "p.created >= :oneDay"),
                 Post.class, Map.of("oneDay", LocalDateTime.now().minusDays(1)));
     }
 
     public List<Post> findPostsWithImage() {
-        return crudRepository.query("SELECT DISTINCT p FROM Post p JOIN FETCH p.history LEFT JOIN FETCH p.participates WHERE p.image IS MOT NULL ORDER BY p.id",
+        return crudRepository.query(String.format(FINDBYCONDITION, "p.image IS MOT NULL"),
                 Post.class);
     }
 
     public List<Post> findPostByCarName(String carName) {
-        return crudRepository.query("SELECT DISTINCT p FROM Post p JOIN FETCH p.history LEFT JOIN FETCH p.participates WHERE lower(p.car.name) LIKE lower(:carName) ORDER BY p.id",
+        return crudRepository.query(String.format(FINDBYCONDITION, "lower(p.car.name) LIKE lower(:carName)"),
                 Post.class, Map.of("carName", '%' + carName + '%'));
     }
 }
